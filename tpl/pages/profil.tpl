099e1b6a1069685f1875a2ed57493b4eb688f1f6
Louka-Millon <Louka06.millon@gmail.com>
2021-03-23 17:38:25 +0100

    front profil

--- tpl/pages/profil.tpl
+++ tpl/pages/profil.tpl
@@ -1,5 +1,11 @@
 <div class="container">
     <div class="row-profil">
-        <img src="" alt="">
+        <!--<img src="../core/images/background-header.png" alt="Profil">-->
+        <h3>Nom du compte</h3>
+        <p>Je suis un étudiant</p>
+
+        <p>Voici ci-joint mon CV</p>
+        <p>Voici ci-joint ma Lettre de motivation</p>
+        <p>Voici nos offres d'emplois</p>
     </div>
 </div>
--- tpl/templates_c/ae323fe5a4d9d151b01b1aaab40502a9c2e80572_0.file.profil.tpl.php
+++ tpl/templates_c/ae323fe5a4d9d151b01b1aaab40502a9c2e80572_0.file.profil.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.39, created on 2021-03-23 14:38:33
+/* Smarty version 3.1.39, created on 2021-03-23 17:35:21
   from 'C:\xamp\htdocs\Entreprise\tpl\pages\profil.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.39',
-  'unifunc' => 'content_6059ef59b9c634_19850905',
+  'unifunc' => 'content_605a18c9340e50_74151897',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     'ae323fe5a4d9d151b01b1aaab40502a9c2e80572' => 
     array (
       0 => 'C:\\xamp\\htdocs\\Entreprise\\tpl\\pages\\profil.tpl',
-      1 => 1616506685,
+      1 => 1616517320,
       2 => 'file',
     ),
   ),
@@ -20,10 +20,15 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_6059ef59b9c634_19850905 (Smarty_Internal_Template $_smarty_tpl) {
+function content_605a18c9340e50_74151897 (Smarty_Internal_Template $_smarty_tpl) {
 ?><div class="container">
     <div class="row-profil">
-        <img src="" alt="">
+        <!--<img src="../core/images/background-header.png" alt="Profil">-->
+        <h3>Nom du compte</h3>
+        <p>Je suis un étudiant</p>
+
+        <p>Voici ci-joint mon CV</p>
+        <p>Voici ci-joint ma Lettre de motivation</p>
     </div>
 </div><?php }
 }
