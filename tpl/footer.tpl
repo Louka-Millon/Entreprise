32d26168a9817f81879d89855f0355b823161982
Lucile-BRIAND <lucilebriand.pro@gmail.com>
2021-03-23 10:43:29 +0100

    Footer organisé + popup cookies

--- tpl/footer.tpl
+++ tpl/footer.tpl
@@ -1,8 +1,21 @@
+<div class="popup-cookies">
+       <div class="popup-mention">
+              <span>Ce site utilise des cookies. Pour plus d'informations, consultez notre politique de confidentialité.</span>
+       </div>
+       <div class="popup-bouton">
+              <button>Accepter</button>
+              <button>En savoir plus</button>
+       </div>
+</div>
 <div class="footer">
-       <a class="footer-item" href="#">Home</a>
-       <a class="footer-item" href="#">Contact</a>
-       <a class="footer-item" href="#">Mentions légales</a>
-       <p>Ce site a été développé par Lucile BRIAND, Nissrine ESSAHLAOUI et Louka MILLON.</p>
+       <div class="footer-block">
+              <a class="footer-item" href="#">Home</a>
+              <a class="footer-item" href="#">Contact</a>
+              <a class="footer-item" href="#">Mentions légales</a>
+       </div>
+       <div class="footer-block">
+              <span>Ce site a été développé par Lucile BRIAND, Nissrine ESSAHLAOUI et Louka MILLON.</span>
+       </div> 
 </div>
 </body>
 </html>
--- tpl/templates_c/bbcc772e05901989785d2d2bc1e1161a343a1c0e_0.file.footer.tpl.php
+++ tpl/templates_c/bbcc772e05901989785d2d2bc1e1161a343a1c0e_0.file.footer.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.39, created on 2021-03-22 15:52:22
+/* Smarty version 3.1.39, created on 2021-03-23 10:33:20
   from 'C:\xampp\htdocs\Entreprise\tpl\footer.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.39',
-  'unifunc' => 'content_6058af26d7c165_04720466',
+  'unifunc' => 'content_6059b5e0ca8ca6_50640827',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     'bbcc772e05901989785d2d2bc1e1161a343a1c0e' => 
     array (
       0 => 'C:\\xampp\\htdocs\\Entreprise\\tpl\\footer.tpl',
-      1 => 1616424741,
+      1 => 1616491951,
       2 => 'file',
     ),
   ),
@@ -20,12 +20,25 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_6058af26d7c165_04720466 (Smarty_Internal_Template $_smarty_tpl) {
-?><div class="footer">
-       <a class="footer-item" href="#">Home</a>
-       <a class="footer-item" href="#">Contact</a>
-       <a class="footer-item" href="#">Mentions légales</a>
-       <p>Ce site a été développé par Lucile BRIAND, Nissrine ESSAHLAOUI et Louka MILLON.</p>
+function content_6059b5e0ca8ca6_50640827 (Smarty_Internal_Template $_smarty_tpl) {
+?><div class="popup-cookies">
+       <div class="popup-mention">
+              <span>Ce site utilise des cookies. Pour plus d'informations, consultez notre politique de confidentialité.</span>
+       </div>
+       <div class="popup-bouton">
+              <button>Accepter</button>
+              <button>En savoir plus</button>
+       </div>
+</div>
+<div class="footer">
+       <div class="footer-block">
+              <a class="footer-item" href="#">Home</a>
+              <a class="footer-item" href="#">Contact</a>
+              <a class="footer-item" href="#">Mentions légales</a>
+       </div>
+       <div class="footer-block">
+              <span>Ce site a été développé par Lucile BRIAND, Nissrine ESSAHLAOUI et Louka MILLON.</span>
+       </div> 
 </div>
 </body>
 </html><?php }
