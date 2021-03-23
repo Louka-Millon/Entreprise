ea653a36c527991644b15983c0132ed74508b199
Lucile-BRIAND <lucilebriand.pro@gmail.com>
2021-03-23 17:53:29 +0100

    Textes et blocs OK

--- tpl/pages/offre.tpl
+++ tpl/pages/offre.tpl
@@ -1,7 +1,9 @@
 <div class="offre-container">
     <div class="offre-row infos">
-        <img src="" alt="IMAGE">
-        <h3>Nom de l'entreprise</h3>
+        <div class="offre-avatar">
+            <img src="https://w.wallhaven.cc/full/j3/wallhaven-j3339m.jpg" alt="IMAGE">
+            <h3>Nom de l'entreprise</h3>
+        </div>
         <p>Localisation</p>
         <p>Coordonnées</p>
         <p>Secteur(s) d'activité :</p>
@@ -20,6 +22,7 @@
     <div class="offre-row details">
         <h2>TITRE DE L'OFFRE</h2>
         <p>Détails de l'offre, durée de l'offre, compétences recherchées, etc...</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non illum odio id veritatis nam facere voluptate adipisci aut dolorem dolor! Beatae necessitatibus facilis quasi, repellendus error ipsa provident atque consectetur! Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus nihil, adipisci autem dolorem nulla quibusdam quaerat labore ut at! Commodi tempora minus, hic nesciunt vero error. Quisquam ab odio deleniti?</p>
         <button>Postuler</button>
     </div>
 </div>
--- tpl/templates_c/fe84582451a153e2b9310acadfd11660ab80035b_0.file.offre.tpl.php
+++ tpl/templates_c/fe84582451a153e2b9310acadfd11660ab80035b_0.file.offre.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.39, created on 2021-03-23 15:23:20
+/* Smarty version 3.1.39, created on 2021-03-23 17:43:54
   from 'C:\xampp\htdocs\Entreprise\tpl\pages\offre.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.39',
-  'unifunc' => 'content_6059f9d881a6f8_20995560',
+  'unifunc' => 'content_605a1acae30951_02741800',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     'fe84582451a153e2b9310acadfd11660ab80035b' => 
     array (
       0 => 'C:\\xampp\\htdocs\\Entreprise\\tpl\\pages\\offre.tpl',
-      1 => 1616509398,
+      1 => 1616517833,
       2 => 'file',
     ),
   ),
@@ -20,11 +20,13 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_6059f9d881a6f8_20995560 (Smarty_Internal_Template $_smarty_tpl) {
+function content_605a1acae30951_02741800 (Smarty_Internal_Template $_smarty_tpl) {
 ?><div class="offre-container">
     <div class="offre-row infos">
-        <img src="" alt="IMAGE">
-        <h3>Nom de l'entreprise</h3>
+        <div class="offre-avatar">
+            <img src="https://w.wallhaven.cc/full/j3/wallhaven-j3339m.jpg" alt="IMAGE">
+            <h3>Nom de l'entreprise</h3>
+        </div>
         <p>Localisation</p>
         <p>Coordonnées</p>
         <p>Secteur(s) d'activité :</p>
@@ -43,6 +45,7 @@ function content_6059f9d881a6f8_20995560 (Smarty_Internal_Template $_smarty_tpl)
     <div class="offre-row details">
         <h2>TITRE DE L'OFFRE</h2>
         <p>Détails de l'offre, durée de l'offre, compétences recherchées, etc...</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non illum odio id veritatis nam facere voluptate adipisci aut dolorem dolor! Beatae necessitatibus facilis quasi, repellendus error ipsa provident atque consectetur! Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus nihil, adipisci autem dolorem nulla quibusdam quaerat labore ut at! Commodi tempora minus, hic nesciunt vero error. Quisquam ab odio deleniti?</p>
         <button>Postuler</button>
     </div>
 </div><?php }
