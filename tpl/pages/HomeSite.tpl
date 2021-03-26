8ac20df9f3ed127298e170aa0a43d057e715b1a1
NissrineEss <nissrine.essahlaoui@viacesi.fr>
2021-03-26 19:12:08 +0100

    HOME OK

--- tpl/pages/HomeSite.tpl
+++ tpl/pages/HomeSite.tpl
@@ -6,9 +6,9 @@
    
     <img src="https://lh3.googleusercontent.com/proxy/LjJXTv8Y6zUlXHYjOF3VxiLdQiGEPKKDAJmCgXPSZ_gVERQkTri32sUlYwmjrIJ12A07iNWNLbDp-JCO8tP-lA2sVz_TaGQd-9CSMpeFnc6dcJ9lFaFyZoYVZ-5RHxu_HWCbYIGjNQ" alt=""  height="200" width="250">
     <img src="https://lh3.googleusercontent.com/proxy/LjJXTv8Y6zUlXHYjOF3VxiLdQiGEPKKDAJmCgXPSZ_gVERQkTri32sUlYwmjrIJ12A07iNWNLbDp-JCO8tP-lA2sVz_TaGQd-9CSMpeFnc6dcJ9lFaFyZoYVZ-5RHxu_HWCbYIGjNQ" alt=""  height="200" width="250">
-
     <img src="https://lh3.googleusercontent.com/proxy/LjJXTv8Y6zUlXHYjOF3VxiLdQiGEPKKDAJmCgXPSZ_gVERQkTri32sUlYwmjrIJ12A07iNWNLbDp-JCO8tP-lA2sVz_TaGQd-9CSMpeFnc6dcJ9lFaFyZoYVZ-5RHxu_HWCbYIGjNQ" alt=""  height="200" width="250">
-
+    <img src="https://lh3.googleusercontent.com/proxy/LjJXTv8Y6zUlXHYjOF3VxiLdQiGEPKKDAJmCgXPSZ_gVERQkTri32sUlYwmjrIJ12A07iNWNLbDp-JCO8tP-lA2sVz_TaGQd-9CSMpeFnc6dcJ9lFaFyZoYVZ-5RHxu_HWCbYIGjNQ" alt=""  height="200" width="250">
+    <img src="https://lh3.googleusercontent.com/proxy/LjJXTv8Y6zUlXHYjOF3VxiLdQiGEPKKDAJmCgXPSZ_gVERQkTri32sUlYwmjrIJ12A07iNWNLbDp-JCO8tP-lA2sVz_TaGQd-9CSMpeFnc6dcJ9lFaFyZoYVZ-5RHxu_HWCbYIGjNQ" alt=""  height="200" width="250">
     <img src="https://lh3.googleusercontent.com/proxy/LjJXTv8Y6zUlXHYjOF3VxiLdQiGEPKKDAJmCgXPSZ_gVERQkTri32sUlYwmjrIJ12A07iNWNLbDp-JCO8tP-lA2sVz_TaGQd-9CSMpeFnc6dcJ9lFaFyZoYVZ-5RHxu_HWCbYIGjNQ" alt=""  height="200" width="250">
    
     <img src="https://lh3.googleusercontent.com/proxy/LjJXTv8Y6zUlXHYjOF3VxiLdQiGEPKKDAJmCgXPSZ_gVERQkTri32sUlYwmjrIJ12A07iNWNLbDp-JCO8tP-lA2sVz_TaGQd-9CSMpeFnc6dcJ9lFaFyZoYVZ-5RHxu_HWCbYIGjNQ" alt=""  height="200" width="250">
@@ -18,33 +18,29 @@
 </div>
   
 <h2>Propositions selon votre domaines</h2>
-<fieldset>
+<fieldset div class="domaine">
     <p style="margin-left: 5%;">Nom du stage</p>
     <p>Localisation &nbsp; &nbsp; Rémunération  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Domaine</p>
-    <div class="bouton">
-        <button id="btn_accéder">Allez à</button>
-    </div>
+    <div style= "position: absolute; margin-left: 90%; background-color: aqua; top: 80%;" class="bouton"> <input type="submit" value="Allez à"> </div>
 </fieldset>
 
-<fieldset>
+<fieldset div class="domaine">
     <p style="margin-left: 5%;">Nom du stage</p>
     <p>Localisation &nbsp; Rémunération &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Domaine</p>
-    <div class="bouton">
-        <button id="btn_accéder">Allez à</button>
-    </div>
+    <div style="position: absolute; margin-left: 90%; background-color: aqua; bottom: 1%" class="bouton"><input type="submit"value="Allez à"></div>
 </fieldset>
 
-<fieldset>
+<fieldset div class="domaine">
     <p style="margin-left: 5%;">Nom du stage</p>
     <p>Localisation &nbsp; Rémunération &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Domaine</p>
-    <div class="bouton">
-        <button id="btn_accéder">Allez à</button>
-    </div>
+    <div style= "position: absolute; margin-left: 90%; border-color: aqua; top: 111%" class="bouton"> <input type="submit" value="Allez à"> </div>
 </fieldset>
 
 <fieldset>
     <p style="margin-left: 5%;" >Nom du stage</p>
     <p>Localisation &nbsp; Rémunération &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Domaine</p>
-    <p> <input type="submit" value="Allez à"> </p>
+    <div style= "position: absolute; margin-left: 90%; background-color: aqua; top: 124%" class="bouton"> <input type="submit" value="Allez à"> </div>
+
 </fieldset>
+
   
--- tpl/templates_c/9bf1684959e870fc2744e66f3640e5f726dd838c_0.file.HomeSite.tpl.php
+++ tpl/templates_c/9bf1684959e870fc2744e66f3640e5f726dd838c_0.file.HomeSite.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.39, created on 2021-03-26 17:48:29
+/* Smarty version 3.1.39, created on 2021-03-26 19:11:13
   from 'C:\xampp\htdocs\Entreprise\tpl\pages\HomeSite.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.39',
-  'unifunc' => 'content_605e105d5b3666_27932311',
+  'unifunc' => 'content_605e23c1ec17b9_43817224',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '9bf1684959e870fc2744e66f3640e5f726dd838c' => 
     array (
       0 => 'C:\\xampp\\htdocs\\Entreprise\\tpl\\pages\\HomeSite.tpl',
-      1 => 1616777266,
+      1 => 1616782272,
       2 => 'file',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_605e105d5b3666_27932311 (Smarty_Internal_Template $_smarty_tpl) {
+function content_605e23c1ec17b9_43817224 (Smarty_Internal_Template $_smarty_tpl) {
 ?><div class="header">
     <h2> Les domaines les plus recherchés</h2>
 </div>
@@ -29,9 +29,9 @@ function content_605e105d5b3666_27932311 (Smarty_Internal_Template $_smarty_tpl)
    
     <img src="https://lh3.googleusercontent.com/proxy/LjJXTv8Y6zUlXHYjOF3VxiLdQiGEPKKDAJmCgXPSZ_gVERQkTri32sUlYwmjrIJ12A07iNWNLbDp-JCO8tP-lA2sVz_TaGQd-9CSMpeFnc6dcJ9lFaFyZoYVZ-5RHxu_HWCbYIGjNQ" alt=""  height="200" width="250">
     <img src="https://lh3.googleusercontent.com/proxy/LjJXTv8Y6zUlXHYjOF3VxiLdQiGEPKKDAJmCgXPSZ_gVERQkTri32sUlYwmjrIJ12A07iNWNLbDp-JCO8tP-lA2sVz_TaGQd-9CSMpeFnc6dcJ9lFaFyZoYVZ-5RHxu_HWCbYIGjNQ" alt=""  height="200" width="250">
-
     <img src="https://lh3.googleusercontent.com/proxy/LjJXTv8Y6zUlXHYjOF3VxiLdQiGEPKKDAJmCgXPSZ_gVERQkTri32sUlYwmjrIJ12A07iNWNLbDp-JCO8tP-lA2sVz_TaGQd-9CSMpeFnc6dcJ9lFaFyZoYVZ-5RHxu_HWCbYIGjNQ" alt=""  height="200" width="250">
-
+    <img src="https://lh3.googleusercontent.com/proxy/LjJXTv8Y6zUlXHYjOF3VxiLdQiGEPKKDAJmCgXPSZ_gVERQkTri32sUlYwmjrIJ12A07iNWNLbDp-JCO8tP-lA2sVz_TaGQd-9CSMpeFnc6dcJ9lFaFyZoYVZ-5RHxu_HWCbYIGjNQ" alt=""  height="200" width="250">
+    <img src="https://lh3.googleusercontent.com/proxy/LjJXTv8Y6zUlXHYjOF3VxiLdQiGEPKKDAJmCgXPSZ_gVERQkTri32sUlYwmjrIJ12A07iNWNLbDp-JCO8tP-lA2sVz_TaGQd-9CSMpeFnc6dcJ9lFaFyZoYVZ-5RHxu_HWCbYIGjNQ" alt=""  height="200" width="250">
     <img src="https://lh3.googleusercontent.com/proxy/LjJXTv8Y6zUlXHYjOF3VxiLdQiGEPKKDAJmCgXPSZ_gVERQkTri32sUlYwmjrIJ12A07iNWNLbDp-JCO8tP-lA2sVz_TaGQd-9CSMpeFnc6dcJ9lFaFyZoYVZ-5RHxu_HWCbYIGjNQ" alt=""  height="200" width="250">
    
     <img src="https://lh3.googleusercontent.com/proxy/LjJXTv8Y6zUlXHYjOF3VxiLdQiGEPKKDAJmCgXPSZ_gVERQkTri32sUlYwmjrIJ12A07iNWNLbDp-JCO8tP-lA2sVz_TaGQd-9CSMpeFnc6dcJ9lFaFyZoYVZ-5RHxu_HWCbYIGjNQ" alt=""  height="200" width="250">
@@ -41,35 +41,31 @@ function content_605e105d5b3666_27932311 (Smarty_Internal_Template $_smarty_tpl)
 </div>
   
 <h2>Propositions selon votre domaines</h2>
-<fieldset>
+<fieldset div class="domaine">
     <p style="margin-left: 5%;">Nom du stage</p>
     <p>Localisation &nbsp; &nbsp; Rémunération  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Domaine</p>
-    <div class="bouton">
-        <button id="btn_accéder">Allez à</button>
-    </div>
+    <div style= "position: absolute; margin-left: 90%; background-color: aqua; top: 80%;" class="bouton"> <input type="submit" value="Allez à"> </div>
 </fieldset>
 
-<fieldset>
+<fieldset div class="domaine">
     <p style="margin-left: 5%;">Nom du stage</p>
     <p>Localisation &nbsp; Rémunération &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Domaine</p>
-    <div class="bouton">
-        <button id="btn_accéder">Allez à</button>
-    </div>
+    <div style="position: absolute; margin-left: 90%; background-color: aqua; bottom: 1%" class="bouton"><input type="submit"value="Allez à"></div>
 </fieldset>
 
-<fieldset>
+<fieldset div class="domaine">
     <p style="margin-left: 5%;">Nom du stage</p>
     <p>Localisation &nbsp; Rémunération &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Domaine</p>
-    <div class="bouton">
-        <button id="btn_accéder">Allez à</button>
-    </div>
+    <div style= "position: absolute; margin-left: 90%; border-color: aqua; top: 111%" class="bouton"> <input type="submit" value="Allez à"> </div>
 </fieldset>
 
 <fieldset>
     <p style="margin-left: 5%;" >Nom du stage</p>
     <p>Localisation &nbsp; Rémunération &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Domaine</p>
-    <p> <input type="submit" value="Allez à"> </p>
+    <div style= "position: absolute; margin-left: 90%; background-color: aqua; top: 124%" class="bouton"> <input type="submit" value="Allez à"> </div>
+
 </fieldset>
+
   
 <?php }
 }
