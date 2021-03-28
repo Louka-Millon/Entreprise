18993ec82e4ba29317cc2125bca03cc89e5c98a7
Louka-Millon <Louka06.millon@gmail.com>
2021-03-28 13:16:53 +0200

    connexion

--- tpl/pages/connexion.tpl
+++ tpl/pages/connexion.tpl
@@ -1,18 +1,16 @@
 {include file='header_other.tpl'}
 
-<div class= "header">
-    <h2>Connectez-vous </h2>
+<div class="container-inscription">
+    <div class="row-titre">
+        <h2>Connexion</h2>
     </div>
-    <div class = "container">
-    <form method="POST" action="connexion.php">
-        <fieldset>
-        <p style="text-align: center;"> Veuillez vous connectez avec votre compte:</p>
-    <label style="text-align: center;">E-mail:</label> 
-     <input style="text-align: center" type="text" placeholder="Entrer votre e-mail" name="username" required> <br> <br> <br> <br> <br>
-     <label style="text-align: center;">Password:</label>
-     <input style="text-align: center" type="Password" placeholder="Entrer votre password" name="password" required>
-    <p style="text-align: center;"> <input type="submit" id="submit" value="Connexion"> </p>
-     </fieldset>
-
-    </form>
+    <div class="row-form">
+        <form action="connexion.php">
+            <p>E-mail :</p>
+            <input type="email" name="mail">
+            <p>Mot de passe :</p>
+            <input type="password" name="pass">
+            <input type="submit" value="Connexion" id="btn_connexion">
+        </form>
+    </div>
 </div>
--- tpl/templates_c/359cd61de1ea29c7a6e2b2a795d813c31fbcd091_0.file.connexion.tpl.php
+++ tpl/templates_c/359cd61de1ea29c7a6e2b2a795d813c31fbcd091_0.file.connexion.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.39, created on 2021-03-27 16:41:06
+/* Smarty version 3.1.39, created on 2021-03-28 13:16:31
   from 'C:\xamp\htdocs\Entreprise\tpl\pages\connexion.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.39',
-  'unifunc' => 'content_605f52126183f4_44746007',
+  'unifunc' => 'content_6060658f419f70_20468651',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '359cd61de1ea29c7a6e2b2a795d813c31fbcd091' => 
     array (
       0 => 'C:\\xamp\\htdocs\\Entreprise\\tpl\\pages\\connexion.tpl',
-      1 => 1616855310,
+      1 => 1616930180,
       2 => 'file',
     ),
   ),
@@ -21,24 +21,22 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
     'file:header_other.tpl' => 1,
   ),
 ),false)) {
-function content_605f52126183f4_44746007 (Smarty_Internal_Template $_smarty_tpl) {
+function content_6060658f419f70_20468651 (Smarty_Internal_Template $_smarty_tpl) {
 $_smarty_tpl->_subTemplateRender('file:header_other.tpl', $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
 ?>
 
-<div class= "header">
-    <h2>Connectez-vous </h2>
+<div class="container-inscription">
+    <div class="row-titre">
+        <h2>Connexion</h2>
     </div>
-    <div class = "container">
-    <form method="POST" action="connexion.php">
-        <fieldset>
-        <p style="text-align: center;"> Veuillez vous connectez avec votre compte:</p>
-    <label style="text-align: center;">E-mail:</label> 
-     <input style="text-align: center" type="text" placeholder="Entrer votre e-mail" name="username" required> <br> <br> <br> <br> <br>
-     <label style="text-align: center;">Password:</label>
-     <input style="text-align: center" type="Password" placeholder="Entrer votre password" name="password" required>
-    <p style="text-align: center;"> <input type="submit" id="submit" value="Connexion"> </p>
-     </fieldset>
-
-    </form>
+    <div class="row-form">
+        <form action="connexion.php">
+            <p>E-mail :</p>
+            <input type="email" name="mail">
+            <p>Mot de passe :</p>
+            <input type="password" name="pass">
+            <input type="submit" value="Connexion" id="btn_connexion">
+        </form>
+    </div>
 </div><?php }
 }
